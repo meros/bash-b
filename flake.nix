b2e1539bba99fb6eab28bcf9f9dc242750e6e00b
Alexander Schrab <alexander.schrab@gmail.com>
2026-01-27 08:04:42 +0100

    Make it a flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Interactive Git branch selector using fuzzy finding";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        git-branch-selector = pkgs.callPackage ./default.nix { };
+      in
+      {
+        packages = {
+          default = git-branch-selector;
+          git-branch-selector = git-branch-selector;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${git-branch-selector}/bin/b";
+          meta = git-branch-selector.meta;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            fzf
+            bash
+          ];
+        };
+
+        checks = {
+          package = git-branch-selector;
+        };
+      }
+    );
+}
